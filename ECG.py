9b3f57ca3b05cb8d0c94b920c9f28aaa5dd8675a
SukrutN <sukrutnadigotti@gmail.com>
2022-11-15 13:41:20 -0500

    Add ECG calculations for all wave sections

--- ECG.py
+++ ECG.py
@@ -96,3 +96,87 @@ else:
        a_uwav = float(input('amplitude = '))
        d_uwav = float(input('duration = '))
        t_uwav = 0.433
+ 
+def p_wav(x, a, d, t, li):
+   l = 1
+   x = x + (1/1.8)
+   b = 3
+   n = 100
+   p1 = 1/l
+   p2 = 0
+   for i in range(1, n+1):
+       harm1 = (((np.sin((np.pi/(2*b))*(b-(2*i))))/(b-(2*i))+(np.sin((np.pi/(2*b))*(b+(2*i))))/(b+(2*i)))*(2/np.pi))*np.cos((i*np.pi*x)/l)
+       p2 = p2 + harm1
+   pwav1 = p1 + p2
+   pwav = a * pwav1
+   return pwav
+ 
+def q_wav(x, a, d, t, li):
+   l = 1
+   x = x + l/6
+   b = 15
+   n = 100
+   q1 = (a/(2*b))*(2-b)
+   q2 = 0
+   for i in range(1, n+1):
+       harm5 = (((2*b*a)/(i*i*np.pi*np.pi))*(1-np.cos((i*np.pi)/b)))*np.cos((i*np.pi*x)/l)
+       q2 = q2 + harm5
+   qwav = -1*(q1 + q2)
+   return qwav
+ 
+def qrs_wav(x, a, d, li):
+   l = 1
+   b = 5
+   n = 100
+   qrs1 = (a/(2*b))*(2-b)
+   qrs2 = 0
+   for i in range(1, n+1):
+       harm = (((2*b*a)/(i*i*np.pi*np.pi))*(1-np.cos((i*np.pi)/b)))*np.cos((i*np.pi*x)/l)
+       qrs2 = qrs2 + harm
+   qrswav = qrs1 + qrs2
+   return qrswav
+ 
+def s_wav(x, a, d, t, li):
+   l = 1
+   x = x - l/6
+   b = 15
+   n = 100
+   s1 = (a/(2*b))*(2-b)
+   s2 = 0
+   for i in range(1, n+1):
+       harm3 = (((2*b*a)/(i*i*np.pi*np.pi))*(1-np.cos((i*np.pi)/b)))*np.cos((i*np.pi*x)/l)
+       s2 = s2 + harm3
+   swav = -1*(s1 + s2)
+   return swav
+ 
+def t_wav(x, a, d, t, li):
+   l = 1
+   a = 0.35
+   x = x - (1/1.8)
+   b = 7
+   n = 20
+   t1 = 1/l
+   t2 = 0
+   for i in range(1, n+1):
+       harm2 = (((np.sin((np.pi/(2*b))*(b-(2*i))))/(b-(2*i))+(np.sin((np.pi/(2*b))*(b+(2*i))))/(b+(2*i)))*(2/np.pi))*np.cos((i*np.pi*x)/l)
+       t2 = t2 + harm2
+   twav1 = t1 + t2
+   twav = a * twav1
+   return twav
+ 
+def u_wav(x, a, d, t, li):
+   l = 1
+   a = 0.03
+   x = x - (1/1.1)
+   b = 21
+   n = 100
+   u1 = 1/l
+   u2 = 0
+   for i in range(1, n+1):
+       harm4 = (((np.sin((np.pi/(2*b))*(b-(2*i))))/(b-(2*i))+(np.sin((np.pi/(2*b))*(b+(2*i))))/(b+(2*i)))*(2/np.pi))*np.cos((i*np.pi*x)/l)
+       u2 = u2 + harm4
+   uwav1 = u1 + u2
+   uwav = a * uwav1
+   return uwav
+ 
+ 
